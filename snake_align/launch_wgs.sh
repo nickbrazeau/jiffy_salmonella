163f765e5f9e15845c0b197baee98dff1b80238a
nickbrazeau <nbrazeau@med.unc.edu>
2022-06-14 23:06:54 -0400

    fix paths

--- snake_align/launch_wgs.sh
+++ snake_align/launch_wgs.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-ROOT=//proj/ideel/meshnick/users/NickB/Projects/jiffy_salmonella/snake_align # root directory for project (non-scratch)
+ROOT=/proj/ideel/meshnick/users/NickB/Projects/jiffy_salmonella/snake_align # root directory for project (non-scratch)
 WD=/pine/scr/n/f/nfb/Projects/jiffy_salmonella/snake_align # working directory for alignments (scratch)
 NODES=1028 # max number of cluster nodes
 WAIT=30 # number of seconds to wait for files to appear, absorbing some file system latency
