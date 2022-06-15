bb79498d4f521d1907fe847f14ba7526e57a43f5
nickbrazeau <nbrazeau@med.unc.edu>
2022-06-15 06:19:35 -0400

    fix paths

--- snake_varcall/launch_wgs.sh
+++ snake_varcall/launch_wgs.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-ROOT=/proj/ideel/meshnick/users/NickB/Projects/jiffy_salmonella/raw_variants
+ROOT=/proj/ideel/meshnick/users/NickB/Projects/jiffy_salmonella/snake_varcall
 WD=/pine/scr/n/f/nfb/Projects/jiffy_salmonella/vcfs_gatk_joint_raw/
 NODES=1028 # max number of cluster nodes
 WAIT=30 # number of seconds to wait for files to appear, absorbing some file system latency
